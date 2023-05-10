82bde4b7961a22eff2040913a8d9b0c3a0fcbb70
Vla Pol <tango2002@gmail.com>
2023-05-10 20:23:40 +0300

    fix: divide sql into 2 files

---
+++ src/main/resources/db/migration/V2__dml_and_dql.sql
@@ -0,0 +1,105 @@
+INSERT INTO topic (topic_title)
+VALUES ('ENGLISH');
+INSERT INTO topic (topic_title)
+VALUES ('PROGRAMMING');
+INSERT INTO topic (topic_title)
+VALUES ('CHEMISTRY');
+
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('COLOR', 1);
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('ANIMAL', 1);
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('VEGETABLES', 1);
+
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('JAVA', 2);
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('C', 2);
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('PYTHON', 2);
+
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('ORGANIC', 3);
+INSERT INTO subtopic (subtopic_title, topic_id)
+VALUES ('INORGANIC', 3);
+
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('RED', 'КРАСНЫЙ', false, 1);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('BLACK', 'ЧЕРНЫЙ', false, 1);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('YELLOW', 'ЖЕЛТЫЙ', false, 1);
+
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('DOG', 'СОБАКА', true, 2);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('CAT', 'КОТ', true, 2);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('HORSE', 'ЛОШАДЬ', false, 2);
+
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('TOMATO', 'ТОМАТ', false, 3);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('POTATO', 'КАРТОФЕЛЬ', false, 3);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('PEPPER', 'ПЕРЕЦ', false, 3);
+
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('ARRAY', 'МАССИВ', false, 4);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('LIST', 'ЛИСТ', false, 4);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('ABSTRACTION', 'АБСТРАКЦИЯ', false, 4);
+
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('REFERENCE', 'ССЫЛКА', false, 5);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('STRUCT', 'СТРУКТУРА', false, 5);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('EOF', 'КОНЕЦ ФАЙЛА', false, 5);
+
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('С2H5OH', 'СПИРТ', false, 7);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('H2O', 'ВОДА', false, 7);
+
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('H2', 'ВОДОРОД', false, 8);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('Au', 'ЗОЛОТО', false, 8);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('Na', 'НАТРИЙ', false, 8);
+INSERT INTO quiz (question, answer, is_remembered, subtopic_id)
+VALUES ('Zn', 'ЦИНК', false, 8);
+
+
+SELECT *
+FROM topic;
+
+SELECT *
+FROM subtopic
+WHERE topic_id = 1;
+
+SELECT *
+FROM quiz q
+WHERE q.is_remembered = 'true';
+
+SELECT COUNT(*)
+FROM quiz
+WHERE is_remembered = 'true';
+
+SELECT t.topic_title, st.subtopic_title, COUNT(q.question) AS total_questions
+FROM topic t
+         LEFT JOIN subtopic st on t.topic_id = st.topic_id
+         LEFT JOIN quiz Q on st.subtopic_id = Q.subtopic_id
+GROUP BY t.topic_title, st.subtopic_title
+ORDER BY topic_title;
+
+SELECT t.topic_title, st.subtopic_title, COUNT(q.is_remembered) AS ready
+FROM topic t
+         LEFT JOIN subtopic st on t.topic_id = st.topic_id
+         LEFT JOIN quiz Q on st.subtopic_id = Q.subtopic_id
+WHERE q.is_remembered = 'true'
+GROUP BY t.topic_title, st.subtopic_title
+ORDER BY topic_title;
