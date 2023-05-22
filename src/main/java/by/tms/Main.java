e9ed38d013385d1d78f834c41f2676a7d3b9b48c
Vla Pol <tango2002@gmail.com>
2023-05-22 19:53:55 +0300

    feat: add connection pool and logback

--- src/main/java/by/tms/Main.java
+++ src/main/java/by/tms/Main.java
@@ -2,65 +2,66 @@ package by.tms;
 
 import by.tms.repository.CardRepositoryImpl;
 import by.tms.utils.DBConnection;
+import com.zaxxer.hikari.HikariDataSource;
 
-import javax.sql.DataSource;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        DataSource dataSource = DBConnection.getConnection();
-        CardRepositoryImpl ri = new CardRepositoryImpl(dataSource);
+        try (HikariDataSource hds = new HikariDataSource(DBConnection.getHikariConfig())) {
+            CardRepositoryImpl ri = new CardRepositoryImpl(hds);
 
-        System.out.println("Список тем");
-        ri.getTopics().stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("Список тем с количеством выученных и общим количеством");
-        ri.getTopicsWithCounts().stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("id темы");
-        Scanner scanner = new Scanner(System.in);
-        Long topicId = Long.parseLong(scanner.nextLine());
-        System.out.println("Список вопросов по теме");
-        ri.getAllQuiz(topicId).stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("Проверка знаний. Введите id темы");
-        topicId = Long.parseLong(scanner.nextLine());
-        System.out.println("Введите смещение");
-        int offset = Integer.parseInt(scanner.nextLine());
-        System.out.println("Список вопросов по теме");
-        ri.getCardByIdAndOffset(topicId, offset).stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("Введите новую тему");
-        String topic = scanner.nextLine();
-        ri.addNewTopic(topic);
-        ri.getTopics().stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("Введите id темы");
-        topicId = Long.parseLong(scanner.nextLine());
-        System.out.println("Введите вопрос");
-        String question = scanner.nextLine();
-        System.out.println("Введите ответ");
-        String answer = scanner.nextLine();
-        boolean isRemembered = false;
-        ri.addNewQuiz(topicId, question, answer, isRemembered);
-        ri.getAllQuiz(4L).stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("Введите id темы для удаления");
-        topicId = Long.parseLong(scanner.nextLine());
-        ri.removeTopic(topicId);
-        ri.getTopics().stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("Введите id задания для удаления");
-        Long quizId = Long.parseLong(scanner.nextLine());
-        ri.removeQuiz(quizId);
-        ri.getTopics().stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
-        System.out.println("Введите id задания которое знаем");
-        quizId = Long.parseLong(scanner.nextLine());
-        ri.updateQuizIsRememberedToTrue(quizId);
-        ri.getAllQuiz(3L).stream().forEach(System.out::println);
-        System.out.println("--------------------------------");
+            System.out.println("Список тем");
+            ri.getTopics().stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("Список тем с количеством выученных и общим количеством");
+            ri.getTopicsWithCounts().stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("id темы");
+            Scanner scanner = new Scanner(System.in);
+            Long topicId = Long.parseLong(scanner.nextLine());
+            System.out.println("Список вопросов по теме");
+            ri.getAllQuiz(topicId).stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("Проверка знаний. Введите id темы");
+            topicId = Long.parseLong(scanner.nextLine());
+            System.out.println("Введите смещение");
+            int offset = Integer.parseInt(scanner.nextLine());
+            System.out.println("Список вопросов по теме");
+            ri.getCardByIdAndOffset(topicId, offset).stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("Введите новую тему");
+            String topic = scanner.nextLine();
+            ri.addNewTopic(topic);
+            ri.getTopics().stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("Введите id темы");
+            topicId = Long.parseLong(scanner.nextLine());
+            System.out.println("Введите вопрос");
+            String question = scanner.nextLine();
+            System.out.println("Введите ответ");
+            String answer = scanner.nextLine();
+            boolean isRemembered = false;
+            ri.addNewQuiz(topicId, question, answer, isRemembered);
+            ri.getAllQuiz(4L).stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("Введите id темы для удаления");
+            topicId = Long.parseLong(scanner.nextLine());
+            ri.removeTopic(topicId);
+            ri.getTopics().stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("Введите id задания для удаления");
+            Long quizId = Long.parseLong(scanner.nextLine());
+            ri.removeQuiz(quizId);
+            ri.getTopics().stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+            System.out.println("Введите id задания которое знаем");
+            quizId = Long.parseLong(scanner.nextLine());
+            ri.updateQuizIsRememberedToTrue(quizId);
+            ri.getAllQuiz(3L).stream().forEach(System.out::println);
+            System.out.println("--------------------------------");
+        }
 
 
     }
--- src/main/java/by/tms/utils/DBConnection.java
+++ src/main/java/by/tms/utils/DBConnection.java
@@ -1,19 +1,20 @@
 package by.tms.utils;
 
+import com.zaxxer.hikari.HikariConfig;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 
 public class DBConnection {
 
-    private DBConnection(){};
-
-    public static DataSource getConnection() {
-        PGSimpleDataSource connection = new PGSimpleDataSource();
-        connection.setUrl(System.getenv("PG_CONNECTION"));
-        connection.setUser(System.getenv("PG_USER"));
-        connection.setPassword(System.getenv("PG_PASSWORD"));
-        return connection;
+    private DBConnection(){}
+    public static HikariConfig getHikariConfig(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(System.getenv("PG_CONNECTION"));
+        hikariConfig.setUsername(System.getenv("PG_USER"));
+        hikariConfig.setPassword(System.getenv("PG_PASSWORD"));
+        return hikariConfig;
     }
+
 
 }
